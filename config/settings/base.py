9b50d717682c567515bf9590e3af9da7dc2081ed
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2026-08-29 16:58:55 +0530

    log dir

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,9 +1,11 @@
 # config/settings/base.py
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
+LOG_DIR = os.path.join(BASE_DIR, "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
 # Application definition
 INSTALLED_APPS = [
     "django.contrib.admin",
